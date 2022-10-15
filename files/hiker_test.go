8aa752e42e73e3e2ee77a6255638aa27463ee177
xALNvc <xALNvc@cyber-dojo.org>
2022-10-15 15:48:00 +0000

    0 kata creation

---
+++ files/hiker_test.go
@@ -0,0 +1,10 @@
+package hiker
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func Test_life_the_universe_and_everything(t *testing.T) {
+    assert.Equal(t, 42, answer())
+}
